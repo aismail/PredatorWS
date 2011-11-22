258d292f38dcd1c61dc1e929c8d4f42b9e02a028
Iacov Andra <iacov.andra.e@gmail.com>
2011-11-22 22:55:34 +0200

    Added a new C++ port created by Georg Nebehay

---
+++ gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/Main.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/Main.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/config.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/config.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/export.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/export.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/gui.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/gui.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/imacq/imAcq.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/imacq/imAcq.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/settings.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/settings.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/Clustering.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/Clustering.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/DetectionResult.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/DetectionResult.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/DetectorCascade.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/DetectorCascade.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/EnsembleClassifier.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/EnsembleClassifier.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/ForegroundDetector.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/ForegroundDetector.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/MedianFlowTracker.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/MedianFlowTracker.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/NNClassifier.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/NNClassifier.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/TLD.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/TLD.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/TLDUtil.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/TLDUtil.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/VarianceFilter.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/VarianceFilter.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/tracker/bb.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/tracker/bb.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/tracker/bb_predict.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/tracker/bb_predict.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/tracker/fbtrack.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/tracker/fbtrack.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/tracker/lk.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/tracker/lk.cpp.o"
+  "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/src/tld/tracker/median.cpp" "/Users/andra/Documents/PredatorWS/gnebehay-OpenTLD/build/src/CMakeFiles/tld.dir/tld/tracker/median.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
